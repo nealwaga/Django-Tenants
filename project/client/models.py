f838862755bf181b122112b3788609d77cae23ed
nealwaga <waganeal@gmail.com>
2023-03-06 11:23:21 +0300

    created models

--- project/client/models.py
+++ project/client/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
 
 # Create your models here.
+class Client(TenantMixin):
+    name = models.CharField(max_length=100)
+
+    auto_create_schema = True
+
+
+class Domain(DomainMixin):
+    pass
