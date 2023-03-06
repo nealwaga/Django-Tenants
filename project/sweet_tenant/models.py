5350b7b0bc5461f79d62ff6c97b8cd082b474ec7
nealwaga <waganeal@gmail.com>
2023-03-06 12:58:07 +0300

    added models

--- project/sweet_tenant/models.py
+++ project/sweet_tenant/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from sweet_shared.models import SweetType
 
 # Create your models here.
+class Sweet(models.Model):
+    sweet_type = models.ForeignKey(SweetType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
